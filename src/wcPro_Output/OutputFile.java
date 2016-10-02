c0804cd02891b17803263f26059e9b4dea6aa62d
ldisthebest <981824007@qq.com>
2016-10-02 23:40:05 +0800

    初始化了工程

---
+++ src/wcPro_Output/OutputFile.java
@@ -0,0 +1,21 @@
+package wcPro_Output;
+
+import java.util.*;
+
+
+public class OutputFile {
+	public static void main (String[] args){
+		List<Map.Entry<String,Integer>> res=new ArrayList<Map.Entry<String,Integer>>();
+		Map.Entry<String,Integer> m;
+		Map<String,Integer> m1 = null;
+		m1.put("as", 1);
+		//res.add(m1);
+	}
+	public void OutPut(List<Map.Entry<String,Integer>> res)
+	{
+		for(int i = 0;i<res.size();i++){
+			
+		}
+	}
+
+}
